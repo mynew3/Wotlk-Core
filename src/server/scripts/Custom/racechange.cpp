1040350fe072fb7bd021c970f2f75faed51459d2
Exitare <raphael.kirchgaessner@gmail.com>
2015-08-06 16:33:16 +0200

    [Custom] Racechange cost

--- src/server/scripts/Custom/racechange.cpp
+++ src/server/scripts/Custom/racechange.cpp
@@ -5,10 +5,10 @@ public:
 
 	bool OnGossipHello(Player* player, Creature* creature)
 	{
-		player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Wie funktioniert das?", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 0);
-		player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Rassenwechsel Kosten: Umsonst.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
-		player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Fraktionswechsel Kosten: Umsonst.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
-		player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Fraktions & Rassenwechsel Kosten: Umsonst.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);
+		player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Wie funktioniert das?", GOSSIP_SENDER_MAIN, 0);
+		player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Rassenwechsel Kosten: 500 Gold.", GOSSIP_SENDER_MAIN,1);
+		player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Fraktionswechsel Kosten: 500 Gold.", GOSSIP_SENDER_MAIN, 2);
+		player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Fraktions & Rassenwechsel Kosten: 500 Gold.", GOSSIP_SENDER_MAIN,3);
 		player->PlayerTalkClass->SendGossipMenu(1, creature->GetGUID());
 		return true;
 	}
@@ -18,37 +18,72 @@ public:
 			return false;
 		switch (action){
 
-		case GOSSIP_ACTION_INFO_DEF + 0:
-			pPlayer->GetGUID();
-			ChatHandler(pPlayer->GetSession()).PSendSysMessage("Einfach auswaehlen was du moechtest und dann reloggen.",
-				pPlayer->GetName());
-			pPlayer->PlayerTalkClass->SendCloseGossip();
-			return true;
+		case  0:
+			
+				pPlayer->GetGUID();
+				ChatHandler(pPlayer->GetSession()).PSendSysMessage("Einfach auswaehlen was du moechtest und dann reloggen.",
+					pPlayer->GetName());
+				pPlayer->PlayerTalkClass->SendCloseGossip();
+				return true;
 			break;
-		case GOSSIP_ACTION_INFO_DEF + 1:
-			pPlayer->SetAtLoginFlag(AT_LOGIN_CHANGE_RACE);
-			pPlayer->GetGUID();
-			ChatHandler(pPlayer->GetSession()).PSendSysMessage("Bitte ausloggen um Aenderungen durchzufuehren.",
-				pPlayer->GetName());
-			pPlayer->PlayerTalkClass->SendCloseGossip();
-			return true;
+		case  1:
+			if (pPlayer->HasItemCount(49426, 4)){
+				pPlayer->DestroyItemCount(49426, 4, true, false);
+				pPlayer->SetAtLoginFlag(AT_LOGIN_CHANGE_RACE);
+				pPlayer->GetGUID();
+				ChatHandler(pPlayer->GetSession()).PSendSysMessage("Bitte ausloggen um Aenderungen durchzufuehren.",
+					pPlayer->GetName());
+				pPlayer->ModifyMoney(-5000000);
+				pPlayer->PlayerTalkClass->SendCloseGossip();
+				return true;
+			}
+
+			else{
+				pPlayer->GetSession()->SendAreaTriggerMessage("Du hast nicht genug Marken zum wechseln. Komm wieder wenn du genug hast.");
+				return true;
+			}
+
 			break;
-		case GOSSIP_ACTION_INFO_DEF + 2:
-			pPlayer->SetAtLoginFlag(AT_LOGIN_CHANGE_FACTION);
-			pPlayer->GetGUID();
-			ChatHandler(pPlayer->GetSession()).PSendSysMessage("Bitte ausloggen um Aenderungen durchzufuehren.",
-				pPlayer->GetName());
-			pPlayer->PlayerTalkClass->SendCloseGossip();
+		case  2:
+
+			if (pPlayer->HasItemCount(49426, 4)){
+				pPlayer->DestroyItemCount(49426, 4, true, false);
+				pPlayer->SetAtLoginFlag(AT_LOGIN_CHANGE_FACTION);
+				pPlayer->GetGUID();
+				ChatHandler(pPlayer->GetSession()).PSendSysMessage("Bitte ausloggen um Aenderungen durchzufuehren.",
+					pPlayer->GetName());
+				pPlayer->ModifyMoney(-5000000);
+				pPlayer->PlayerTalkClass->SendCloseGossip();
+				return true;
+			}
+
+
+		else{
+			pPlayer->GetSession()->SendAreaTriggerMessage("Du hast nicht genug Marken zum wechseln. Komm wieder wenn du genug hast.");
 			return true;
+		}
+
 			break;
-		case GOSSIP_ACTION_INFO_DEF + 3:
-			pPlayer->SetAtLoginFlag(AT_LOGIN_CHANGE_FACTION);
-			pPlayer->SetAtLoginFlag(AT_LOGIN_CHANGE_RACE);
-			pPlayer->GetGUID();
-			ChatHandler(pPlayer->GetSession()).PSendSysMessage("Bitte ausloggen um Änderungen durchzuführen.",
-				pPlayer->GetName());
-			pPlayer->PlayerTalkClass->SendCloseGossip();
-			return true;
+		case 3:
+			if (pPlayer->HasItemCount(49426, 4)){
+				pPlayer->DestroyItemCount(49426, 4, true, false);
+				pPlayer->SetAtLoginFlag(AT_LOGIN_CHANGE_FACTION);
+				pPlayer->SetAtLoginFlag(AT_LOGIN_CHANGE_RACE);
+				pPlayer->GetGUID();
+				ChatHandler(pPlayer->GetSession()).PSendSysMessage("Bitte ausloggen um Änderungen durchzuführen.",
+					pPlayer->GetName());
+				pPlayer->ModifyMoney(-5000000);
+				pPlayer->PlayerTalkClass->SendCloseGossip();
+				return true;
+			}
+
+
+			else{
+				pPlayer->GetSession()->SendAreaTriggerMessage("Du hast nicht genug Marken zum wechseln. Komm wieder wenn du genug hast.");
+				return true;
+			}
+
+
 			break;
 		}
 		return true;
