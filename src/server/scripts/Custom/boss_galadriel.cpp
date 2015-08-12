d334b90c4f26df3a20b1d1ef55ab5511d6fa90e2
Exitare <raphael.kirchgaessner@gmail.com>
2015-08-12 14:48:37 +0200

    [Custom] Test some

--- src/server/scripts/Custom/boss_galadriel.cpp
+++ src/server/scripts/Custom/boss_galadriel.cpp
@@ -109,8 +109,8 @@ public:
 		{
 			/*char msg[250];
 			snprintf(msg, 250, "|cffff0000[Boss System]|r Boss|cffff6060 Galadriel|r wurde getötet! Der Respawn ist in 7 Tagen ab jetzt! ", player->GetName()); 
-			sWorld->SendGlobalText(msg, NULL);
-			Summons.DespawnAll();*/
+			sWorld->SendGlobalText(msg, NULL);*/
+			Summons.DespawnAll();
 		}
 
 		void UpdateAI(uint32 diff) override
